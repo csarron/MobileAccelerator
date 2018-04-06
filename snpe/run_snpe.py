1b74c4fcda50b1e1d3f4f11256fc9586a8799aa7
Arron Cao <qqcao.cs@gmail.com>
2018-04-05 21:03:32 -0400

    add: snpe one shot script

---
+++ snpe/run_snpe.py
@@ -0,0 +1,284 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import argparse
+import numpy as np
+import os
+import tensorflow as tf
+import zipfile as zp
+import subprocess
+import glob
+import json
+from PIL import Image
+from collections import OrderedDict
+import shutil
+import stat
+
+
+def convert_to_dlc(script_path, frozen_model_file, input_node='input', output_node='output', image_size=224):
+    print('converting ' + frozen_model_file + ' to snpe dlc format')
+    dlc_path = frozen_model_file.replace('.pb', '.dlc')
+    if os.path.exists(dlc_path):
+        return dlc_path
+    st = os.stat(script_path)
+    os.chmod(script_path, st.st_mode | stat.S_IEXEC)
+    cmd = [script_path,
+           '--graph', frozen_model_file,
+           '--input_dim', input_node, '{0},{0},3'.format(image_size),
+           '--out_node', output_node,
+           '--allow_unconsumed_nodes',
+           '--dlc', dlc_path]
+    subprocess.call(cmd)
+    return dlc_path
+    # print('INFO: Creating ' + DLC_QUANTIZED_FILENAME + ' quantized model')
+    # data_cropped_dir = os.path.join(os.path.join(model_dir, 'data'), 'cropped')
+    # cmd = ['snpe-dlc-quantize',
+    #        '--input_dlc', os.path.join(dlc_dir, DLC_FILENAME),
+    #        '--input_list', os.path.join(data_cropped_dir, RAW_LIST_FILE),
+    #        '--output_dlc', os.path.join(dlc_dir, DLC_QUANTIZED_FILENAME)]
+    # subprocess.call(cmd)
+
+
+def __get_img_raw(img_file):
+    img_file = os.path.abspath(img_file)
+    img = Image.open(img_file)
+    img_ndarray = np.array(img)  # read it
+    if len(img_ndarray.shape) != 3:
+        raise RuntimeError('Image shape' + str(img_ndarray.shape))
+    if img_ndarray.shape[2] != 3:
+        raise RuntimeError('Require image with rgb but channel is %d' % img_ndarray.shape[2])
+    # reverse last dimension: rgb -> bgr
+    return img_ndarray
+
+
+def __create_mean_raw(img_raw, mean_rgb):
+    if img_raw.shape[2] != 3:
+        raise RuntimeError('Require image with rgb but channel is %d' % img_raw.shape[2])
+    img_dim = (img_raw.shape[0], img_raw.shape[1])
+    mean_raw_r = np.empty(img_dim)
+    mean_raw_r.fill(mean_rgb[0])
+    mean_raw_g = np.empty(img_dim)
+    mean_raw_g.fill(mean_rgb[1])
+    mean_raw_b = np.empty(img_dim)
+    mean_raw_b.fill(mean_rgb[2])
+    # create with c, h, w shape first
+    tmp_transpose_dim = (img_raw.shape[2], img_raw.shape[0], img_raw.shape[1])
+    mean_raw = np.empty(tmp_transpose_dim)
+    mean_raw[0] = mean_raw_r
+    mean_raw[1] = mean_raw_g
+    mean_raw[2] = mean_raw_b
+    # back to h, w, c
+    mean_raw = np.transpose(mean_raw, (1, 2, 0))
+    return mean_raw.astype(np.float32)
+
+
+def __create_raw_img(img_file, mean_rgb, div, req_bgr_raw, save_uint8):
+    img_raw = __get_img_raw(img_file)
+    mean_raw = __create_mean_raw(img_raw, mean_rgb)
+
+    snpe_raw = img_raw - mean_raw
+    snpe_raw = snpe_raw.astype(np.float32)
+    # scalar data divide
+    snpe_raw /= div
+
+    if req_bgr_raw:
+        snpe_raw = snpe_raw[..., ::-1]
+
+    if save_uint8:
+        snpe_raw = snpe_raw.astype(np.uint8)
+    else:
+        snpe_raw = snpe_raw.astype(np.float32)
+
+    img_file = os.path.abspath(img_file)
+    filename, ext = os.path.splitext(img_file)
+    snpe_raw_filename = filename
+    snpe_raw_filename += '.raw'
+    snpe_raw.tofile(snpe_raw_filename)
+
+    return 0
+
+
+def __resize_square_to_jpg(src, dst, size):
+    src_img = Image.open(src)
+    # If black and white image, convert to rgb (all 3 channels the same)
+    if len(np.shape(src_img)) == 2: src_img = src_img.convert(mode='RGB')
+    # center crop to square
+    width, height = src_img.size
+    short_dim = min(height, width)
+    crop_coord = (
+        (width - short_dim) / 2,
+        (height - short_dim) / 2,
+        (width + short_dim) / 2,
+        (height + short_dim) / 2
+    )
+    img = src_img.crop(crop_coord)
+    # resize to alexnet size
+    dst_img = img.resize((size, size), Image.ANTIALIAS)
+    # save output - save determined from file extension
+    dst_img.save(dst)
+    return 0
+
+
+def convert_img(src, dest, size):
+    print("converting images...")
+    print("scaling to square: " + src)
+    for root, dirs, files in os.walk(src):
+        for jpgs in files:
+            src_image = os.path.join(root, jpgs)
+            if '.jpg' in src_image:
+                print(src_image)
+                dest_image = os.path.join(dest, jpgs)
+                __resize_square_to_jpg(src_image, dest_image, size)
+
+    print("image mean: " + dest)
+    for root, dirs, files in os.walk(dest):
+        for jpgs in files:
+            src_image = os.path.join(root, jpgs)
+            print(src_image)
+            mean_rgb = (128, 128, 128)
+            __create_raw_img(src_image, mean_rgb, 128, False, False)
+
+
+def create_file_list(input_dir, output_filename, ext_pattern, print_out=False, rel_path=False):
+    input_dir = os.path.abspath(input_dir)
+    output_filename = os.path.abspath(output_filename)
+    output_dir = os.path.dirname(output_filename)
+
+    if not os.path.isdir(input_dir):
+        raise RuntimeError('input_dir %s is not a directory' % input_dir)
+
+    if not os.path.isdir(output_dir):
+        raise RuntimeError('output_filename %s directory does not exist' % output_dir)
+
+    glob_path = os.path.join(input_dir, ext_pattern)
+    file_list = glob.glob(glob_path)
+
+    if rel_path:
+        file_list = [os.path.relpath(file_path, output_dir) for file_path in file_list]
+
+    if len(file_list) <= 0:
+        if print_out:
+            print('no results with %s' % glob_path)
+    else:
+        with open(output_filename, 'w') as f:
+            f.write('\n'.join(file_list))
+            if print_out:
+                print('%s created listing %d files.' % (output_filename, len(file_list)))
+
+
+def prepare_data_images(image_size, snpe_root, data_dir_):
+    # make a copy of the image files from the alexnet model data dir
+    src_img_files = os.path.join(snpe_root, 'models', 'alexnet', 'data', '*.jpg')
+    image_dir = os.path.join(data_dir_, 'images')
+    data_cropped_dir = os.path.join(image_dir, 'cropped')
+    raw_list = os.path.join(image_dir, 'target_raw_list_%s.txt' % image_size)
+
+    if not os.path.isdir(data_cropped_dir):
+        os.makedirs(data_cropped_dir)
+
+        for image in glob.glob(src_img_files):
+            shutil.copy(image, image_dir)
+        print('creating inception style raw image data')
+        convert_img(image_dir, data_cropped_dir, image_size)
+
+        print('Create file lists')
+        if not os.path.exists(raw_list):
+            create_file_list(data_cropped_dir, raw_list, '*.raw', print_out=True, rel_path=False)
+
+    return data_cropped_dir, raw_list
+
+
+# generate bench config json file
+def gen_config(dlc_path, input_list_file, input_data):
+    name = os.path.splitext(os.path.basename(dlc_path))[0]
+    config = OrderedDict()
+    config['Name'] = name
+    config['HostRootPath'] = name
+    config['HostResultsDir'] = os.path.join(name, 'results')
+    config['DevicePath'] = '/data/local/tmp/snpebm'
+    config['Devices'] = ["123"]
+    config['Runs'] = 2
+
+    model = OrderedDict()
+    model['Name'] = name
+    model['Dlc'] = dlc_path
+    model['InputList'] = input_list_file
+    model['Data'] = [input_data]
+    config['Model'] = model
+
+    config['Runtimes'] = ['GPU', 'CPU']
+    config['Measurements'] = ['timing', 'mem']
+
+    return config
+
+
+def write_config(config, save_path):
+    with open(save_path, 'w') as f:
+        json.dump(config, f, indent=4)
+
+
+# run snpe bench script
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sdk", "--snpe_sdk", type=str, default="data/snpe-1.13.0.zip",
+                        help="path to snpe sdk zip file")
+    parser.add_argument("-m", "--model", type=str, default="data/mobilenet_v1/mobilenet_v1_1.0_224.frozen.pb",
+                        help="frozen tensorflow model")
+    parser.add_argument("-s", "--image_size", type=int, default=224,
+                        help="input image size")
+    parser.add_argument("-i", "--input_node", type=str, default='input',
+                        help="input node name in the model")
+    parser.add_argument("-o", "--output_node", type=str, default='output',
+                        help="output node name in the model")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    web_url = "https://developer.qualcomm.com/software/snapdragon-neural-processing-engine-ai"
+    tf_path = os.path.dirname(tf.__file__)
+
+    args = parse_args()
+
+    snpe_sdk_file = args.snpe_sdk
+
+    snpe_dir = os.path.dirname(snpe_sdk_file)
+    snpe_sdk_path = os.path.abspath(os.path.splitext(snpe_sdk_file)[0])
+
+    if not os.path.exists(snpe_sdk_file):
+        print("please download SNPE SDK from:", web_url)
+        exit(-1)
+    elif not os.path.exists(snpe_sdk_path):
+        print("extracting snpe to:", snpe_sdk_path, "...")
+        zp_ref = zp.ZipFile(snpe_sdk_file, 'r')
+        zp_ref.extractall(snpe_dir)
+        zp_ref.close()
+        print("snpe sdk extraction done.")
+    else:
+        print("found snpe sdk at:", snpe_sdk_path)
+    print()
+
+    os.environ["SNPE_ROOT"] = snpe_sdk_path
+    py_path = os.environ.get("PYTHONPATH", "")
+    os.environ["PYTHONPATH"] = "{0}/lib/python:{1}".format(snpe_sdk_path, py_path)
+
+    os.environ["TENSORFLOW_HOME"] = tf_path
+    bin_path = os.environ.get("PATH", "")
+    os.environ["PATH"] = "{}/bin/x86_64-linux-clang:{}".format(snpe_sdk_path, bin_path)
+
+    model_file = args.model
+    if not os.path.exists(model_file):
+        print(model_file, "not exist!")
+        exit(-1)
+
+    # print(os.environ["PATH"])
+    convert_dlc_script = "{}/bin/x86_64-linux-clang/snpe-tensorflow-to-dlc".format(snpe_sdk_path)
+    dlc_file = convert_to_dlc(convert_dlc_script, model_file, args.input_node, args.output_node, args.image_size)
+
+    data_dir, raw_file_list = prepare_data_images(args.image_size, snpe_sdk_path, "data")
+
+    config = gen_config(dlc_file, raw_file_list, data_dir)
+    config_path = "{}.json".format(model_file)
+    write_config(config, config_path)
+
+    bench_cmd = ['python', 'snpe_bench.py', '-c', os.path.abspath(config_path), '-a']
+    subprocess.call(bench_cmd, cwd='{}/benchmarks'.format(snpe_sdk_path))
