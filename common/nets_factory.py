65848b7854f0240a063d4a47fc55f650ca4850dd
Arron Cao <qqcao.cs@gmail.com>
2018-05-12 02:10:21 -0400

    add: fw nets

--- common/nets_factory.py
+++ common/nets_factory.py
@@ -42,6 +42,8 @@ from syn_nets import twoalexnet
 from syn_nets import twosqueezenet
 from syn_nets.var_alexnet import alexnet_bs
 from syn_nets.var_alexnet import alexnet_bs2
+from syn_nets.var_alexnet import alexnet_fw1
+from syn_nets.var_alexnet import alexnet_fw5
 
 slim = tf.contrib.slim
 
@@ -166,3 +168,7 @@ networks_map['alexnet_bs'] = alexnet_bs.alexnet_bs
 arg_scopes_map['alexnet_bs'] = alexnet_bs.alexnet_bs_arg_scope
 networks_map['alexnet_bs2'] = alexnet_bs2.alexnet_bs2
 arg_scopes_map['alexnet_bs2'] = alexnet_bs2.alexnet_bs2_arg_scope
+networks_map['alexnet_fw1'] = alexnet_fw1.alexnet_fw1
+arg_scopes_map['alexnet_fw1'] = alexnet_fw1.alexnet_fw1_arg_scope
+networks_map['alexnet_fw5'] = alexnet_fw5.alexnet_fw5
+arg_scopes_map['alexnet_fw5'] = alexnet_fw5.alexnet_fw5_arg_scope
