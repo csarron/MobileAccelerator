b1afea1f38d8b5d45dbe2f93f6bc0a0cc2755323
Alexandru Irimiea <alexandru.irim@gmail.com>
2019-10-10 05:07:40 +0300

    Added command line arguments

--- ncs2/run_custom_nets.py
+++ ncs2/run_custom_nets.py
@@ -2,20 +2,32 @@ import os
 import sys
 sys.path.append('../')
 
-outputDir = "./large_filters"
+from argparse import ArgumentParser, SUPPRESS
+
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS,
+        help='Show this help message and exit.')
+    args.add_argument("-e", '--experiments_dir', required=True, type=str)
+    return parser
 
 def executeCommand(command):
     #print(command)
     os.system(command)
 
 def executeModels(min, max, step):
+    outputDir = args.experiments_dir
     for i in range(min, max, step):
-        # index = str(i).zfill(6)
-        # file_name_stem = outputDir + '/inception_v3_' + index
-        file_name_stem = outputDir + '/inception_v3_' + str(i)
+        file_name_stem = outputDir + '/inception_v3_' + str(i).zfill(6)
         xml_file = file_name_stem + '.frozen.xml'
 
         command = 'echo NCS2:%TIME% & python classification_sample.py --model ' + xml_file + ' --input car.jpg --device MYRIAD --perf_counts --number_iter 5'
         executeCommand(command)
 
-executeModels(384 + 256, 4096, 256)
+def main():
+    executeModels(1024 + 256, 2048, 64)
+
+if __name__ == '__main__':
+    args = build_argparser().parse_args()
+    sys.exit(main() or 0)
