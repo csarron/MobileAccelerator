b83941e090b1d5c78b3a93c110fb45ef1847383a
Alexandru Irimiea <alexandru.irim@gmail.com>
2019-10-15 07:35:06 +0300

    Saving create_tf_models

--- ncs2/create_tf_models.py
+++ ncs2/create_tf_models.py
@@ -33,23 +33,36 @@ slim = tf.contrib.slim
 
 #     return model
 
-def generate_models(min, max, step):
-    for kernel_size in range(min, max, step):
-        model_name = 'model_input' + str(28) + 'x' + str(28) \
-            + '_filter' + str(kernel_size) + 'x' + str(kernel_size) + '.pb'
+# def generate_models(min, max, step):
+#     for kernel_size in range(min, max, step):
+#         model_name = 'model_input' + str(28) + 'x' + str(28) \
+#             + '_filter' + str(kernel_size) + 'x' + str(kernel_size) + '.pb'
 
-        # model = create_model(args.input_size, args.input_size, kernel_size, kernel_size)
-        # model = create_model(28, 28, kernel_size, kernel_size)
+#         # model = create_model(args.input_size, args.input_size, kernel_size, kernel_size)
+#         # model = create_model(28, 28, kernel_size, kernel_size)
 
-
-
-        frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name
-             for out in model.outputs])
-        tf.train.write_graph(frozen_graph, output_dir, model_name, as_text=False)
+#         frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name
+#              for out in model.outputs])
+#         tf.train.write_graph(frozen_graph, output_dir, model_name, as_text=False)
 
 def generate_model1():
-    model = tf.keras.models.Sequential()
-    model.add(layers.Conv2D(256, (3, 3), input_shape=(224, 224, 3)))
+    # model = tf.keras.models.Sequential()
+    # model.add(layers.Conv2D(256, (3, 3), input_shape=(224, 224, 3)))
+
+    end_points = {}
+    end_point = 'Mixed_6a'
+    with tf.variable_scope(end_point):
+        with tf.variable_scope('Branch_0'):
+            branch_0 = slim.conv2d(net, 512, [3, 3], stride=2, padding='VALID',scope='Conv2d_1a_1x1')
+        with tf.variable_scope('Branch_1'):
+            branch_1 = slim.conv2d(net, depth(64), [1, 1], scope='Conv2d_0a_1x1')
+            branch_1 = slim.conv2d(branch_1, depth(96), [3, 3], scope='Conv2d_0b_3x3')
+            branch_1 = slim.conv2d(branch_1, depth(96), [3, 3], stride=2, padding='VALID', scope='Conv2d_1a_1x1')
+        with tf.variable_scope('Branch_2'):
+            branch_2 = slim.max_pool2d(net, [3, 3], stride=2, padding='VALID', scope='MaxPool_1a_3x3')
+        net = tf.concat(axis=3, values=[branch_0, branch_1, branch_2])
+        end_points[end_point] = net
+        if end_point == final_endpoint: return net, end_points
 
     model.compile(optimizer=tf.keras.optimizers.Adam(),
         loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
@@ -57,8 +70,8 @@ def generate_model1():
     frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name
             for out in model.outputs])
 
-    tf.train.write_graph(frozen_graph, 'small_nets', 'model1.pb', as_text=False)
-    convert_model('small_nets/model1.pb')
+    tf.train.write_graph(frozen_graph, './', 'model2.pb', as_text=False)
+    convert_model('./model2.pb')
 
 def main():
     # log.getLogger().setLevel(log.INFO)
